89dde898c391fea8945178a0d7bc894d4d4ac539
Roy Desktop <leekokbing.12@gmail.com>
2017-01-20 13:20:27 +0800

    Updated to latest version of website

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -19,9 +19,9 @@ gulp.task('watch', function() {
     gulp.start('cssInject');
   });
 
-    watch('./app/assets/scripts/**/*.js', function() {
-        gulp.start('scriptsRefresh');
-    })
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
 
 });
 
@@ -31,5 +31,5 @@ gulp.task('cssInject', ['styles'], function() {
 });
 
 gulp.task('scriptsRefresh', ['scripts'], function() {
-    browserSync.reload();
+  browserSync.reload();
 });
