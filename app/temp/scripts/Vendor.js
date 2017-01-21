60398801b8567c2bf655a1d36e629f635002c6cf
Roy Desktop <leekokbing.12@gmail.com>
2017-01-21 18:29:52 +0800

    Completed build task

--- app/temp/scripts/Vendor.js
+++ app/temp/scripts/Vendor.js
@@ -293,10 +293,10 @@
 	   *   elem.style.WebkitBorderRadius
 	   * instead of something like the following, which would be technically incorrect:
 	   *   elem.style.webkitBorderRadius
-	    * Webkit ghosts their properties in lowercase but Opera & Moz do not.
+	     * Webkit ghosts their properties in lowercase but Opera & Moz do not.
 	   * Microsoft uses a lowercase `ms` instead of the correct `Ms` in IE8+
 	   *   erik.eae.net/archives/2008/03/10/21.48.10/
-	    * More here: github.com/Modernizr/Modernizr/issues/issue/21
+	     * More here: github.com/Modernizr/Modernizr/issues/issue/21
 	   *
 	   * @access private
 	   * @returns {string} The string representing the vendor-specific style properties
